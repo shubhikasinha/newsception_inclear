0f02bd6cdad2e0b162d4d37e6d93836ffeee0480
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-11-08 23:40:39 +0530

    integrated tailwind setup

--- newsception_inclear/app/page.tsx
+++ newsception_inclear/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             🎤 Join Debate Room
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs"
             target="_blank"
             rel="noopener noreferrer"
