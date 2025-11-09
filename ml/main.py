e00a4c2d015ab4ab1f46be296032d959ab44fd01
Satyamzz <satyamsingh18177@gmail.com>
2025-11-09 10:07:19 +0530

    cleaned

--- ml/main.py
+++ ml/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-from summarizer import router
+from summarizer import app as sum
+from sentimentanalysis import app as sent
 
-app = FastAPI()
-app.include_router(router)
+main_app = FastAPI(title="Combined FastAPI Apps")
+
+# Mount both apps under different paths
+main_app.mount(sum)
+main_app.mount(sent)
+
+@main_app.get("/")
+def root():
+    return {"message": "Main FastAPI is running"}
---
+++ newsception_inclear/testing_api.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'https://newsapi.org/v2/everything'
+params = {
+    'q': 'Israel Palestine',
+    'language': 'en',
+    'apiKey': 'baf024c72e8a4315a310a2f83a1025c0'
+}
+res = requests.get(url, params=params)
+articles = res.json()['articles']
+
+for a in articles[:5]:
+    print(a)
